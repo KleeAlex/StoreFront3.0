938c791d19f0047d8b98c6fdf04ec3a3559dadb9
Alex Klee <kleealex34@outlook.com>
2022-03-28 18:09:03 -0500

    Add project files.

---
+++ StoreFront3.0.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoreFront3._0.UI.MVC.MvcApplication" Language="C#" %>
---
+++ StoreFront3.0.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreFront3._0.UI.MVC.Startup))]
+namespace StoreFront3._0.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
