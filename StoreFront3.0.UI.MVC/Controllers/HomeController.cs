7ba16fef6c35ee8417e54157585de84b46265e14
Alex Klee <kleealex34@outlook.com>
2022-04-03 16:15:57 -0500

    Cart working, unhappy with the way I had to do it, but first comes fucntion then comes style. Cart Commit

--- StoreFront3.0.UI.MVC/Controllers/HomeController.cs
+++ StoreFront3.0.UI.MVC/Controllers/HomeController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Drawing;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using StoreFront3._0.UI.MVC;
+using StoreFront3._0.UI.MVC.Models;
+using StoreFront.DATA.EF;
 
 namespace StoreFront.Controllers
 {
     public class HomeController : Controller
     {
+
+        private DCAccessoriesEntities1 db = new DCAccessoriesEntities1();
+
         public ActionResult Index()
         {
             return View();
@@ -34,14 +44,67 @@ namespace StoreFront.Controllers
             return View();
         }
 
-        public ActionResult SingleItem()
-        {
-            ViewBag.Message = "Well here it is!";
+       
 
-            return View();
+
+        #region Custom Add-to-Cart Functionality
+
+        public ActionResult AddToCart(int qty, int productID)
+        {
+            //Create an empty shell for the LOCAL shopping cart variable
+            Dictionary<int, CartItemViewModel> shoppingCart = null;
+
+            //Check if the Session shopping cart exists. If so, use it to populate the local version
+            if (Session["cart"] != null)
+            {
+                //Session shopping cart exists. Put its items in the local version, which is easier to work with
+                shoppingCart = (Dictionary<int, CartItemViewModel>)Session["cart"];
+                //We need to UNBOX the Session object to its smaller, more specific type -- Explicit casting
+            }
+            else
+            {
+                //If the Session cart doesn't exist yet, we need to instantiate it to get started
+                shoppingCart = new Dictionary<int, CartItemViewModel>();
+            }//After this if/else, we now have a local cart that's ready to add things to it
+
+            //Find the product they referenced by its ID
+            Product product = db.Products.Where(p => p.ProductID == productID).FirstOrDefault();
+
+            if (product == null)
+            {
+                //If given a bad ID, return the user to some other page to try again.
+                //Alternatively, we could throw some kind of error, which we will 
+                //discuss further in Module 6.
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //If the Book IS valid, add the line-item to the cart
+                CartItemViewModel item = new CartItemViewModel(qty, product);
+
+                //Put the item in the local cart. If they already have that product as a
+                //cart item, the instead we will update the quantity. This is a big part
+                //of why we have the dictionary.
+                if (shoppingCart.ContainsKey(product.ProductID))
+                {
+                    shoppingCart[product.ProductID].Qty += qty;
+                }
+                else
+                {
+                    shoppingCart.Add(product.ProductID, item);
+                }
+
+                //Now update the SESSION version of the cart so we can maintain that info between requests
+                Session["cart"] = shoppingCart; //No explicit casting needed here
+
+            }
+
+            //Send them to View their Cart Items
+            return RedirectToAction("Index", "ShoppingCart");
+
         }
 
-
+        #endregion
 
     }
 }
